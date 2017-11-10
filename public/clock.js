a5309826f8dddd95e2084106154e5659dc649bc7
Torquoal <sevendaysinthesun@gmail.com>
2017-11-10 00:18:01 +0000

    added stylesheet

--- public/clock.js
+++ public/clock.js
@@ -1,35 +1,35 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2.5;
+var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90
 setInterval(drawClock, 1000);
 
 function drawClock() {
-  drawFace(ctx, radius);
-  drawNumbers(ctx, radius);
-  drawTime(ctx, radius);
+drawFace(ctx, radius);
+drawNumbers(ctx, radius);
+drawTime(ctx, radius);
 }
 
 function drawFace(ctx, radius) {
-  var grad;
-  ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2*Math.PI);
-  ctx.fillStyle = 'white';
-  ctx.fill();
-  ctx.beginPath();
-  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-  ctx.fillStyle = '#333';
-  ctx.fill();
+var grad;
+ctx.beginPath();
+ctx.arc(0, 0, radius, 0, 2*Math.PI);
+ctx.fillStyle = 'white';
+ctx.fill();
+ctx.beginPath();
+ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+ctx.fillStyle = '#333';
+ctx.fill();
 }
 
 function drawNumbers(ctx, radius) {
-  var ang;
-  var num;
-  ctx.font = radius*0.15 + "px arial";
-  ctx.textBaseline="middle";
-  ctx.textAlign="center";
-  for(num = 1; num < 13; num++){
+var ang;
+var num;
+ctx.font = radius*0.15 + "px arial";
+ctx.textBaseline="middle";
+ctx.textAlign="center";
+for(num = 1; num < 13; num++){
     ang = num * Math.PI / 6;
     ctx.rotate(ang);
     ctx.translate(0, -radius*0.85);
@@ -38,7 +38,7 @@ function drawNumbers(ctx, radius) {
     ctx.rotate(ang);
     ctx.translate(0, radius*0.85);
     ctx.rotate(-ang);
-  }
+}
 }
 
 function drawTime(ctx, radius){
@@ -54,7 +54,7 @@ function drawTime(ctx, radius){
     drawHand(ctx, hour, radius*0.5, radius*0.07);
     //minute
     minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    drawHand(ctx, minute, radius*0.8, radius*0.06);
     // second
     second=(second*Math.PI/30);
     drawHand(ctx, second, radius*0.9, radius*0.02);
